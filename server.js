d2d1b0e01e5236637daa5b5180a021538464eea0
Edmond Del Mundo <edmond.delmundo@gmail.com>
2020-11-16 20:35:36 +0800

    test

--- server.js
+++ server.js
@@ -72,6 +72,6 @@ app.get("/signout", auth.requireAuth, (req, res) => {
   signout(req, res);
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("app is running on port 3000");
 });
