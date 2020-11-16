63ca34f6ed59175bbeede19ccc20e16317fba416
Edmond Del Mundo <edmond.delmundo@gmail.com>
2020-11-16 20:11:25 +0800

    make it better

--- controllers/redis.js
+++ controllers/redis.js
@@ -1,7 +1,14 @@
 const redis = require("redis");
 const { promisify } = require("util");
 
-const redisClient = redis.createClient(process.env.REDIS_URI);
+let redisClient = null;
+
+if (process.env.ENV === "production") {
+  redisClient = redis.createClient(process.env.REDIS_URL);
+} else {
+  redisClient = redis.createClient(process.env.REDIS_URI);
+}
+
 const redisClientGet = promisify(redisClient.get).bind(redisClient);
 const redisClientSet = promisify(redisClient.set).bind(redisClient);
 const redisClientDel = promisify(redisClient.del).bind(redisClient);
